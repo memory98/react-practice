4f51040aefc1b6359dd1e258e601b2f39e72eb28
memory98 <asdlkjh252567@gmail.com>
2023-03-15 13:25:10 +0900

    kanbanboard #2

--- kanbanboard/frontend/src/TaskList.js
+++ kanbanboard/frontend/src/TaskList.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Task from './Task';
 import styles  from './assets/css/TaskList.css';
-const TaskList = ({cardNo,tasks, callbackAddTask}) => {
+const TaskList = ({tasks, callbackAddTask, callbackChangeTaskDone}) => {
 
     return (
         <div>
@@ -10,9 +10,9 @@ const TaskList = ({cardNo,tasks, callbackAddTask}) => {
                     tasks.map(task => <Task
                                     key={task.no}
                                     no={task.no}
-                                    cardNo={cardNo}
                                     name={task.name}
                                     done={task.done}
+                                    callbackChangeTaskDone={callbackChangeTaskDone}
                                     />)
                 }
             </ul>
