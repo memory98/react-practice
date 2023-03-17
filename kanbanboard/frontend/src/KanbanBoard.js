906a2982763a3e1cd7f543ff5dc2829868ea4ec8
memory98 <asdlkjh252567@gmail.com>
2023-03-17 12:31:29 +0900

    .

--- kanbanboard/frontend/src/KanbanBoard.js
+++ kanbanboard/frontend/src/KanbanBoard.js
@@ -1,5 +1,5 @@
 import React,{useEffect, useState} from 'react';
-import styles from './assets/css/kanbanBoard.css';
+import styles from './assets/css/KanbanBoard.css';
 import CardList from './CardList';
 
 const KanbanBoard = () => {
